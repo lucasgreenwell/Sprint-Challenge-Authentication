c547fbe66d93a36d48d1c681254dffb0ef9c0962
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-02-28 13:30:56 -0500

    mvp done, something went wrong with CRA command so the front end doesn't work

--- api/server.js
+++ api/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require('express-session');
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
 
 const server = express();
+
+const sessionConfig = {
+    name: 'bikini',
+    secret: 'super secret',
+    cookie: {
+        maxAge: 1000 * 3600 * 24, //one day in milliseconds
+        secure: false, //needs to be true in production
+        httpOnly: true
+    },
+    resave: false,
+    saveUninitialized: true //needs to be false in production for gdpr compliance, illegal to set cookies automatically
+}
+server.use(session(sessionConfig))
+
 
 server.use(helmet());
 server.use(cors());
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,57 @@
 const router = require('express').Router();
+const cors = require('cors')
+const bcrypt = require('bcryptjs')
+const session = require('express-session');
+
+const db = require('../database/dbConfig');
 
 router.post('/register', (req, res) => {
   // implement registration
+
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({ err: 'include all required fields' })
+  } else {
+
+    const hash = bcrypt.hashSync(req.body.password, 12)
+    req.body.password = hash;
+    db('users').insert(req.body)
+      .then(id => {
+        res.status(201).json(id);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ err: "look, we aren't happy about this either" })
+      })
+  }
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  if (!req.body.username || !req.body.password) {
+    res.status(400).json({ err: 'include all required fields' })
+  } else {
+    db('users').select('*').where({ username: req.body.username })
+      .then(user => { 
+        if (user) {
+          // console.log(user)
+          if (bcrypt.compareSync(req.body.password, user[0].password)) {
+            req.session.user = user[0]
+            req.session.loggedIn = true;
+            res.status(200).json({ message: 'success' })
+          }
+          else {
+            res.status(403).json({ message: 'invalid credentials' })
+          }
+        }
+        else {
+          res.status(403).json({ message: 'invalid credentials' })
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ err: "look, we aren't happy about this either" })
+      })
+  }
 });
 
 module.exports = router;
